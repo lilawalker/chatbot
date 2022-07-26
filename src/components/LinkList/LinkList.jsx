ac14c5435f5f54abb8af4a5130a2938c530ecfd5
lila.walker <lila.walker@and.digital>
2022-07-26 16:21:49 +0100

    remove console log

--- src/components/LinkList/LinkList.jsx
+++ src/components/LinkList/LinkList.jsx
@@ -1,23 +1,17 @@
-import React from "react";
+import React from 'react';
 
-import "./LinkList.css";
+import './LinkList.css';
 
 const LinkList = (props) => {
-  console.log(props);
   const linkMarkup = props.options.map((link) => (
-    <li key={link.id} className="link-list-item">
-      <a
-        href={link.url}
-        target="_blank"
-        rel="noopener noreferrer"
-        className="link-list-item-url"
-      >
+    <li key={link.id} className='link-list-item'>
+      <a href={link.url} target='_blank' rel='noopener noreferrer' className='link-list-item-url'>
         {link.text}
       </a>
     </li>
   ));
 
-  return <ul className="link-list">{linkMarkup}</ul>;
+  return <ul className='link-list'>{linkMarkup}</ul>;
 };
 
 export default LinkList;
